377d8c5222b18891ca1eae6f651c8b92da01b3d6
luan-s <luanjs.if@gmail.com>
2016-04-08 01:18:44 -0300

    Adicionei os leveis

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+/// Função para phonegap
 var app = {
     initialize: function() {
         this.bindEvents();
@@ -22,17 +22,51 @@ var app = {
 };
 
 
+
+levelAtual = 0;
+tempoRodada = 15;
 var vet = [];
 var rodada = 0;
 tentativas = 0;
 var porcent = 0;
-var cores = ['#EEE','#009688','#4caf50','#03a9f4','#ff5722','#f44336'];
-dec = 0;
-min = 1;
-seg = 1;
+var cores = ['#616161','#009688','#4caf50','#03a9f4','#ff5722','#f44336','#9C27B0','#9C27B0','#8BC34A','#795548','#03A9F4'];
+dec = 100;
+min = 0;
+seg = 15;
+tempo = null;
+porcent = 0;
 
-function sorteia(){
-  for(i = 0;i<6;i++){
+v = [1,2,3,4];
+
+
+
+function setLevel(level){
+
+  if(level==1) tempoRodada = 10;
+  
+
+  if(level==2) tempoRodada = 5;
+
+  if(level==3){
+    tempoRodada = 20;
+    v.push(5); v.push(6);
+  }
+
+  if(level==4) tempoRodada = 12;
+
+  if(level==5) tempoRodada = 10;
+
+  if(level==6){
+    tempoRodada = 25;
+    v.push(7); v.push(8);
+  }
+
+  constroi(v);
+}
+
+function sorteia(qtd){
+  vet.length = 0;
+  for(i = 0;i<qtd;i++){
     do{
       achou = 0;
       num = Math.floor(Math.random() * 10);
@@ -41,12 +75,32 @@ function sorteia(){
         if(vet[y] == num)
           achou = 1;
       }
-    }while(num>=7 || achou == 1);
+    }while(num>=qtd+1 || achou == 1);
     vet.push(num);
   }
 }
 
-sorteia();
+function constroi(vet){
+  if(vet.length==4) tam ="160px";
+  if(vet.length==6) tam ="130px";
+  if(vet.length==8) tam ="100px";
+  if(vet.length==10) tam ="80px";
+
+  $(".botoes").empty();
+  for(i = 1; i <= vet.length;i++){
+    $(".botoes").append('<div class="cont col-xs-6"><a href="javascript:void(0)" id="'+i+'" class="btn btn-raised btn-primary nums col-xs-11">'+i+'</a></div>');
+    $("#"+i).css("background-color",cores[i]);
+    $("#1").css("background-color",'#616161');
+    $(".nums").css("height",tam);
+    $(".nums").css("padding",(300/vet.length).toString()+'px');
+    $("#level").text("Level "+(levelAtual+1));
+  }
+
+
+  sorteia(vet.length);
+
+}
+
 
 function restart(){
   porcent = 0 ;
@@ -56,9 +110,22 @@ function restart(){
   $('.app').css('background-color','#f5f5f5');
 }
 
-$('a').click(function(){
+function resultado(r){
+  if(r){
+    var resu = "Parabéns"
+    setLevel(++levelAtual);
+  } 
+  else var resu = "Acabou o tempo!"  
+   $('.content-1').addClass("hidden");
+    $('.content-2').removeClass("hidden"); 
+    $('#tentativas').text("Tentativas: "+tentativas)
+    $('#resu').text(resu)
+    clearInterval(tempo);
+}
+
+$(document).on('click','.nums',function(e) {
   if(tentativas==0){
-    startTime();
+    tempo = startTime(tempoRodada);
     console.log(rodada);
   }
   tentativas+=1;
@@ -67,21 +134,16 @@ $('a').click(function(){
 
   if(indice == vet[rodada]){
     $('#'+indice.toString()).css('transform','scale(0)');
-
-
-    porcent += 16.66;
-    $('.progress-bar').css("width",porcent.toString()+'%');
+    porcent += 100/v.length;
+   // $('.progress-bar').css("width",porcent.toString()+'%');
     rodada++;
     $('.app').css('background-color',cores[indice-1]);
   }else{
     restart();
   }
 
-  if(rodada == 6){
-    $('.content-1').addClass("hidden");
-    $('.content-2').removeClass("hidden"); 
-    $('#tentativas').text("Tentativas: "+tentativas)
-    clearInterval(tempo);
+  if(rodada == vet.length){
+    resultado(1);
   }
 
 })
@@ -90,13 +152,13 @@ $('.restart').click(function(){
   $('.content-1').removeClass("hidden");
   $('.content-2').addClass("hidden");  
   vet.length = 0; 
-  sorteia();
+  sorteia(v.length);
 
-  dec = 0;
-  min = 1;
+  dec = 100;
+  min = 10;
   seg = 1;
 
-  $('.seg').text('00'); 
+  $('.seg').text(tempoRodada); 
   $('.dec').text('00'); 
   $('.min').text('00'); 
   tentativas = 0;
@@ -108,32 +170,35 @@ $('.restart').click(function(){
 
 ///Cronometro
 
-function startTime(){
+function startTime(t){
+  seg = t;
   tempo = setInterval(function(){
     if(dec<10)
-      $('.dec').text('0'+(dec++).toString()); 
+      $('.dec').text('0'+(--dec).toString()); 
     else
-      $('.dec').text((dec++).toString()); 
+      $('.dec').text((--dec).toString()); 
     atualizaTempo();
   }, 10);
+  return tempo;
 }
 
   function atualizaTempo(){
-      if(dec==100){
 
-        if(seg<10)
-           $('.seg').text('0'+(seg++).toString()); 
+      if(dec==0){
+
+        if(seg<=10)
+           $('.seg').text('0'+(--seg).toString()); 
         else
-           $('.seg').text((seg++).toString()); 
-        dec = 0;
+           $('.seg').text((--seg).toString()); 
+        dec = 100;
       }
 
-      if(seg==61){
-        if(min<10)
-          $('.min').text('0'+(min++).toString()); 
-        else
-          $('.min').text((min++).toString()); 
-
-        seg = 0;
+      if(seg==0){
+        resultado(0);
       }
+      var porcent = tempoRodada;
+      porcent = (seg) * 100 / tempoRodada-2;
+      $('.progress-bar').css("width",porcent+'%');
   }
+
+constroi(v);
